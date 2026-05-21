630f30b30a1435fbe6ebf2683752a86c607c0ade
Box5789 <pck279049@gmail.com>
2026-05-22 08:06:56 +0900

    Add PCK test item texture

--- scripts/build-pack.sh
+++ scripts/build-pack.sh
@@ -6,5 +6,5 @@ cd "$(dirname "$0")/.."
 mkdir -p build
 rm -f build/assets.zip build/assets.sha1
 
-zip -qr build/assets.zip pack.mcmeta assets -x "*/.gitkeep"
+zip -qr build/assets.zip pack.mcmeta assets -x "*/.gitkeep" "*/.DS_Store"
 shasum -a 1 build/assets.zip | tee build/assets.sha1
